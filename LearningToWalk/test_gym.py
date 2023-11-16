5016f0d81be4907131ea25def986938a0dbd2911
kenma8 <kwright.m@gmail.com>
2023-11-15 16:24:23 -0800

    setup

---
+++ LearningToWalk/test_gym.py
@@ -0,0 +1,20 @@
+import gymnasium as gym
+from gymnasium.wrappers import HumanRendering
+
+def main():
+	env = HumanRendering(gym.make('Walker2d-v4', render_mode='rgb_array'))
+	observation, info = env.reset()
+
+	for _ in range(10000):
+		action = env.action_space.sample()  # agent policy that uses the observation and info
+		observation, reward, terminated, truncated, info = env.step(action)
+		env.render()
+		"""
+		if terminated or truncated:
+			observation, info = env.reset()
+		"""
+
+	env.close()
+
+if __name__ == "__main__":
+  	main()
