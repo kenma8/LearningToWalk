db9e9383c75a90fc2070eb9048ee49e2910caf5a
kenma8 <kwright.m@gmail.com>
2023-11-22 16:29:41 -0800

    Basic DDPG

--- LearningToWalk/test_gym.py
+++ LearningToWalk/test_gym.py
@@ -9,10 +9,6 @@ def main():
 		action = env.action_space.sample()  # agent policy that uses the observation and info
 		observation, reward, terminated, truncated, info = env.step(action)
 		env.render()
-		"""
-		if terminated or truncated:
-			observation, info = env.reset()
-		"""
 
 	env.close()
 
---
+++ LearningToWalk/train_ddpg.py
@@ -0,0 +1,30 @@
+import gymnasium as gym
+from gymnasium.wrappers import HumanRendering
+import numpy as np
+
+from stable_baselines3 import DDPG
+from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+
+
+def main():
+    env = gym.make('Walker2d-v4', render_mode='rgb_array')
+
+    n_actions = env.action_space.shape[-1]
+    action_noise = NormalActionNoise(mean=np.zeros(n_actions), sigma=0.1 * np.ones(n_actions))
+
+    model = DDPG("MlpPolicy", env, action_noise=action_noise, verbose=1)
+    model.learn(total_timesteps=1000000, log_interval=10)
+    model.save("models/ddpg_walker_1m")
+
+    render_env = HumanRendering(model.get_env())
+    obs = render_env.reset()
+
+    while True:
+        action, _states = model.predict(obs)  
+        obs, rewards, done, info = render_env.step(action)
+        render_env.render()
+
+    render_env.close()
+
+if __name__ == "__main__":
+  	main()
